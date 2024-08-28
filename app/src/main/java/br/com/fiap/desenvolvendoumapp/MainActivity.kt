b3bafb1ee0f016d9cd3d2a9fc77eaec5279cf3d9
fernandoheusi <fernandoj.mhs@gmail.com>
2024-08-28 10:31:15 -0400

    creat app

---
+++ app/src/main/java/br/com/fiap/desenvolvendoumapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.fiap.desenvolvendoumapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.desenvolvendoumapp.ui.theme.DesenvolvendoUmAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DesenvolvendoUmAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DesenvolvendoUmAppTheme {
+        Greeting("Android")
+    }
+}
