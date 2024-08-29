747609fd5f8a408d9520fbb8b57af9d695449a4c
fernandoheusi <fernandoj.mhs@gmail.com>
2024-08-28 20:06:49 -0400

    create Home

--- app/src/main/java/br/com/fiap/desenvolvendoumapp/MainActivity.kt
+++ app/src/main/java/br/com/fiap/desenvolvendoumapp/MainActivity.kt
@@ -4,14 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.desenvolvendoumapp.ui.theme.DesenvolvendoUmAppTheme
+import br.com.fiap.desenvolvendoumapp.ui.theme.Roboto
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +37,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DesenvolvendoUmAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    HomeScreen()
                 }
             }
         }
@@ -31,17 +49,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ){
+        TextField(value = "", onValueChange = {})
+        Text(text = "Futsal")
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF15BA35)),
+        ) {
+
+            Column (
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp, vertical = 6.dp)
+            ){
+                Text(
+                    text = "Futsal na quadra",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    fontFamily = Roboto,
+                    color = Color.White
+                )
+                Text(
+                    text = "Quarta - 17/08 as 09:30",
+                    fontSize = 11.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    fontFamily = Roboto,
+                    color = Color.White
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DesenvolvendoUmAppTheme {
-        Greeting("Android")
-    }
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
--- app/src/main/java/br/com/fiap/desenvolvendoumapp/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/desenvolvendoumapp/ui/theme/Type.kt
@@ -2,10 +2,13 @@ package br.com.fiap.desenvolvendoumapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.desenvolvendoumapp.R
 
+val Roboto = FontFamily(Font(R.font.roboto_bold))
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
