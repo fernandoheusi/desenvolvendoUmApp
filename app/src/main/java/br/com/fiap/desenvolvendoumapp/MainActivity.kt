ff623e60bc9f97a15bb479deac5d6f5114876ed3
fernandoheusi <fernandoj.mhs@gmail.com>
2024-08-30 12:48:59 -0400

    add login screen

--- app/src/main/java/br/com/fiap/desenvolvendoumapp/MainActivity.kt
+++ app/src/main/java/br/com/fiap/desenvolvendoumapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.fiap.desenvolvendoumapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,19 +12,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,7 +52,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White
                 ) {
-                    HomeScreen()
+//                    HomeScreen()
+                    LoginScreen()
                 }
             }
         }
@@ -50,12 +62,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+
+    var text by remember {
+        mutableStateOf("")
+    }
+
     Column (
         modifier = Modifier
             .fillMaxWidth()
             .padding(32.dp)
     ){
-        TextField(value = "", onValueChange = {})
+        TextField(
+            value = text,
+            onValueChange = { letter ->
+                text = letter
+            },
+            modifier = Modifier.fillMaxWidth(),
+//            label= {
+//                Text(text = "Busque por eventos")
+//            },
+            placeholder = {
+                Text(text = "Busque por eventos")
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.magnifying_glass),
+                    contentDescription = "Icone de lupa"
+                )
+            }
+        )
         Text(text = "Futsal")
         Button(
             onClick = { /*TODO*/ },
@@ -92,8 +127,63 @@ fun HomeScreen(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+    var textEmail by remember {
+        mutableStateOf("")
+    }
+
+    var textSenha by remember {
+        mutableStateOf("")
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        TextField(
+            value = textEmail,
+            onValueChange = { letter ->
+                textEmail = letter
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "e-mail")
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.check),
+                    contentDescription = "Verificado"
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            textStyle = TextStyle(color = Color(0xFF1F779E), fontSize = 16.sp)
+        )
+        TextField(
+            value = textSenha,
+            onValueChange = { letter ->
+                textSenha = letter
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "senha")
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.eyecrossed),
+                    contentDescription = "Olho riscado"
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            textStyle = TextStyle(color = Color(0xFF1F779E), fontSize = 16.sp)
+        )
+    }
+}
+
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen()
+    LoginScreen()
 }
