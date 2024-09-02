06193fb584887fe5450e2b144beb0a6768ca531c
Flavio Valle Da Cunha <88721496+flavio-valle@users.noreply.github.com>
2024-09-02 00:53:40 -0300

    add implementation de navegacao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation ("androidx.navigation:navigation-compose:2.6.0")
 }
